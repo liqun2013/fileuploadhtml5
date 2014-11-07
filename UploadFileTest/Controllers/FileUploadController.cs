23849c9d58471a458e4e6b37a29ced8514adf78a
liqun2013 <llq_2007@msn.com>
2014-11-08 00:15:43 +0800

    1108

---
+++ UploadFileTest/Controllers/FileUploadController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace UploadFileTest.Controllers
+{
+	public class FileUploadController : Controller
+	{
+		private string tempExtension = "_temp";
+		private string fileName;
+		private string fileType;
+		private DateTime lastModifiedTime;
+		private long fileSize;
+		private long startRange;
+		private long endRange;
+		private string fileFolder;
+		private string fileFullPath;
+		private string fileTempFullPath;
+
+		protected string FileFolder
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(fileFolder))
+				{
+					fileFolder = Path.Combine("C:\\uploadedfile", DateTime.Now.ToString("yyyyMM"));
+					if (!Directory.Exists(fileFolder))
+					{
+						Directory.CreateDirectory(fileFolder);
+					}
+				}
+
+				return fileFolder;
+			}
+		}
+
+		protected string FileFullPath
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(fileFullPath))
+				{
+					if (!string.IsNullOrWhiteSpace(FileFolder) && !string.IsNullOrWhiteSpace(fileName))
+					{
+						fileFullPath = Path.Combine(fileFolder, fileName);
+					}
+				}
+
+				return fileFullPath;
+			}
+		}
+
+		protected string FileTempFullPath
+		{
+			get
+			{
+				if (string.IsNullOrWhiteSpace(fileTempFullPath))
+				{
+					if (!string.IsNullOrWhiteSpace(FileFullPath))
+					{
+						fileTempFullPath = FileFullPath + tempExtension;
+					}
+				}
+
+				return fileTempFullPath;
+			}
+		}
+
+		public ActionResult UploadFileTest()
+		{
+			return View();
+		}
+
+		[HttpPut]
+		public JsonResult ProcessUpload()
+		{
+			if (this.Request.InputStream.Length == 0)
+			{
+				return Json(new { Error = "无文件" });
+			}
+
+			var headers = this.Request.Headers;
+
+			fileName = headers["Content-FileName"];
+			fileType = headers["Content-FileType"];
+			var fileLastModified = headers["Content-FileLastModified"];
+			var fs = headers["Content-FileSize"];
+			var rs = headers["Content-RangeStart"];
+			var re = headers["Content-RangeEnd"];
+
+			DateTime.TryParse(fileLastModified, out lastModifiedTime);
+			long.TryParse(fs, out fileSize);
+			long.TryParse(rs, out startRange);
+			long.TryParse(re, out endRange);
+
+			if (startRange == 0)
+			{
+				try
+				{
+					if (System.IO.File.Exists(FileFullPath))
+					{
+						System.IO.File.Delete(FileFullPath);
+					}
+					else
+					{
+						if (System.IO.File.Exists(FileTempFullPath))
+						{
+							using (FileStream f = System.IO.File.Open(FileTempFullPath, FileMode.Open))
+							{
+								return Json(new { Success = true, UploadedRange = f.Length });
+							}
+						}
+					}
+				}
+				catch (IOException ioexc)
+				{
+					return Json(new { Error = ioexc.Message });
+				}
+			}
+			else
+			{
+				if (!System.IO.File.Exists(FileTempFullPath))
+				{
+					return Json(new { Success = true, UploadedRange = 0 });
+				}
+			}
+
+			try
+			{
+				using (FileStream f = System.IO.File.Open(FileTempFullPath, FileMode.Append))
+				{
+					SaveFile(this.Request.InputStream, f);
+				}
+
+				if (endRange == fileSize)
+				{
+					System.IO.File.Move(FileTempFullPath, FileFullPath);
+				}
+			}
+			catch (IOException ioExc)
+			{
+				return Json(new { Error = ioExc.Message });
+			}
+
+			return Json(new { Success = true });
+		}
+
+		/// <summary>
+		/// Save the contents of the Stream to a file
+		/// </summary>
+		/// <param name="stream"></param>
+		/// <param name="f"></param>
+		private void SaveFile(Stream stream, FileStream f)
+		{
+			byte[] buffer = new byte[2048];
+			int bytesRead;
+			while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+			{
+				f.Write(buffer, 0, bytesRead);
+			}
+		}
+	}
+}
---
+++ UploadFileTest/Scripts/fileupload.js
@@ -0,0 +1,232 @@
+﻿/**
+ * Utility method to format bytes into the most logical magnitude (KB, MB, or GB).
+ */
+Number.prototype.formatBytes = function () {
+	var units = ['B', 'KB', 'MB', 'GB', 'TB'],
+        bytes = this,
+        i;
+
+	for (i = 0; bytes >= 1024 && i < 4; i++) {
+		bytes /= 1024;
+	}
+
+	return bytes.toFixed(2) + units[i];
+}
+
+$(function () {
+	var upload_form = $('#upload_form'),
+        file_input = $('#selectedFile'),
+        file_list = $('#file_list'),
+        submit_btn = $('#submit_btn'),
+        uploaders = [];
+
+	file_input.on('change', onFilesSelected);
+	upload_form.on('submit', onFormSubmit);
+
+	file_list.delegate("button", "click", onPauseClick);
+
+	/**
+	 * Loops through the selected files, displays their file name and size
+	 * in the file list, and enables the submit button for uploading.
+	 */
+	function onFilesSelected(e) {
+		var files = e.target.files,
+			file,
+			list_item,
+			uploader;
+
+		file_list.empty();
+		uploaders = [];
+
+		for (var i = 0; i < files.length; i++) {
+			file = files[i];
+			uploader = new ChunkedUploader(file);
+			uploaders.push(uploader);
+			list_item = $('<li>' + file.name + '(' + file.size.formatBytes() + ') </li>').data('uploader', uploader);
+			file_list.append(list_item);
+		}
+
+		file_list.show();
+		submit_btn.show();
+	}
+
+	/**
+	 * Toggles pause method of the button's related uploader instance.
+	 */
+	function onPauseClick(e) {
+		var btn = $(this),
+			uploader = btn.parent('li').data('uploader');
+
+		if (btn.hasClass('paused')) {
+			btn.removeClass('paused').text('暂停');
+			uploader.resume();
+		}
+		else {
+			btn.addClass('paused').text('继续');
+			uploader.pause();
+		}
+	}
+	/**
+     * Loops through all known uploads and starts each upload
+     * process, preventing default form submission.
+     */
+	function onFormSubmit(e) {
+		$.each(uploaders, function (i, uploader) {
+			uploader.start();
+		});
+
+		uploaders = [];
+
+		// Prevent default form submission
+		e.preventDefault();
+	}
+});
+
+function ChunkedUploader(file, options) {
+	if (!this instanceof ChunkedUploader) {
+		return new ChunkedUploader(file, options);
+	}
+
+	this.file = file;
+
+	this.options = $.extend({
+		url: 'http://10.1.200.211:8050/FileUpload/ProcessUpload'
+	}, options);
+
+	this.file_size = this.file.size;
+	this.chunk_size = (1024 * 64); // 64KB
+	this.range_start = 0;
+	this.range_end = this.chunk_size;
+
+	if ('mozSlice' in this.file) {
+		this.slice_method = 'mozSlice';
+	}
+	else if ('webkitSlice' in this.file) {
+		this.slice_method = 'webkitSlice';
+	}
+	else {
+		this.slice_method = 'slice';
+	}
+
+	var self = this;
+
+	this.upload_request = new XMLHttpRequest();
+	this.upload_request.onload = function () { self._onChunkComplete(); };
+
+	// Respond to changes in connection
+	if ('onLine' in navigator) {
+		window.addEventListener('online', this._onConnectionFound);
+		window.addEventListener('offline', this._onConnectionLost);
+	}
+}
+
+ChunkedUploader.prototype = {
+
+	_upload: function () {
+		var self = this,
+            chunk;
+
+		// Slight timeout needed here (File read / AJAX readystate conflict?)
+		setTimeout(function () {
+			// Prevent range overflow
+			if (self.range_end > self.file_size) {
+				self.range_end = self.file_size;
+			}
+
+			chunk = self.file[self.slice_method](self.range_start, self.range_end);
+
+			self.upload_request.open('PUT', self.options.url, true);
+			self.upload_request.overrideMimeType('application/octet-stream');
+
+			self.upload_request.setRequestHeader('Content-FileName', self.file.name);
+			self.upload_request.setRequestHeader('Content-FileLastModified', self.file.lastModifiedDate);
+			self.upload_request.setRequestHeader('Content-FileType', self.file.type);
+			self.upload_request.setRequestHeader('Content-FileSize', self.file_size);
+
+			self.upload_request.setRequestHeader('Content-RangeStart', self.range_start);
+			self.upload_request.setRequestHeader('Content-RangeEnd', self.range_end);
+
+			self.upload_request.send(chunk);
+		}, 20);
+	},
+
+	_onChunkComplete: function () {
+
+		var $li = $('#file_list').find("li:contains(" + this.file.name + ")");
+		var $btn = $li.find('button');
+		var $span = $li.find('span');
+		if ($span.length > 0) {
+			$span.text(Math.ceil((this.range_end / this.file_size) * 100) + "%");
+		}
+		else {
+			$btn.before("<span>" + Math.ceil((this.range_end / this.file_size) * 100) + "%</span>");
+		}
+
+
+		// If the end range is already the same size as our file, we
+		// can assume that our last chunk has been processed and exit
+		// out of the function.
+		if (this.range_end === this.file_size) {
+			this._onUploadComplete();
+			return;
+		}
+
+		var resp = JSON.parse(this.upload_request.response);
+
+		if (resp.Error) {
+			return;
+		}
+
+		if (resp.Success) {
+			if (resp.UploadedRange) {
+				this.range_start = resp.UploadedRange;
+				this.range_end = this.range_start + this.chunk_size;
+			}
+			else if (resp.UploadedRange == 0) {
+				this.range_start = resp.UploadedRange;
+				this.range_end = this.range_start + this.chunk_size;
+			}
+			else {
+				this.range_start = this.range_end;
+				this.range_end = this.range_start + this.chunk_size;
+			}
+
+			// Continue as long as we aren't paused
+			if (!this.is_paused) {
+				this._upload();
+			}
+		}
+	},
+
+	_onUploadComplete: function () {
+		var $li = $('#file_list').find("li:contains(" + this.file.name + ")");
+		var $btn = $li.find('button').remove();
+	},
+
+	_onConnectionFound: function () {
+		this.resume();
+	},
+
+	_onConnectionLost: function () {
+		this.pause();
+	},
+
+	start: function () {
+		var $li = $('#file_list').find("li:contains(" + this.file.name + ")");
+		if ($li.find('button').length == 0) {
+			$li.append("<button class='btn btn-default btn-xs'>暂停</button>");
+		}
+
+		this._upload();
+	},
+
+	pause: function () {
+		this.is_paused = true;
+	},
+
+	resume: function () {
+		this.is_paused = false;
+		this._upload();
+	}
+};
+
